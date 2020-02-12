73b2b28e65f9fae64d1cd61fdb1895b6ff7785ff
meirelon <nestelm@gmail.com>
2020-02-12 00:40:39 -0500

    code review before any more changes

--- deps/coin_utils.py
+++ deps/coin_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import cbpro
 
-def get_coin_data(ticker, rolling_period=175):
+def get_coin_data(ticker="btc", rolling_period=175):
     if not (isinstance(ticker, str)):
         raise TypeError('Input must be a string')
     s = 'https://poloniex.com/public?command=returnChartData&currencyPair=USDT_' + ticker.upper() + '&start=1439020800&end=9999999999&period=14400'
@@ -13,25 +14,6 @@ def get_coin_data(ticker, rolling_period=175):
     except:
         print('%s failed' % ticker.upper())
         pass
-
-
-
-def buy_event(df, buy_history=[]):
-    latest_record = len(df)-1
-    if df.iloc[latest_record,:]["close"] <= df.iloc[latest_record,:]["rolling_min"]:
-        buy = 100/df.iloc[latest_record,:]["close"]
-        buy_history.append(buy)
-        return [True, buy_history]
-
-
-
-def sell_event(df, buy_history=[]):
-    latest_record
-    if df.iloc[latest_record,:]["close"] >= df.iloc[latest_record,:]["rolling_max"] and len(buy_history)>0:
-        btc_amount = buy_history.pop(0)
-        sell = btc_amount*df.iloc[latest_record,:]["close"]
-        if sell < 100:
-            buy_history.insert(0,btc_amount)
 
 
 def cbpro_auth(k,s,p):
--- main.py
+++ main.py
@@ -2,37 +2,73 @@ import os
 import pickle
 
 import pandas as pd
-import numpy as np
-
 import cbpro
+
+from google.cloud import storage
+from tempfile import NamedTemporaryFile
 
 
 from deps.utils import upload_blob, load_blob
 import deps.coin_utils as coin_utils
-def crypto_event():
+def crypto_event(request):
     project = os.environ["PROJECT_ID"]
+    bucket = os.environ["GCS_BUCKET"]
 
     key = os.environ["CBPRO_KEY"]
     secret = os.environ["CBPRO_SECRET"]
     passphrase = os.environ["CBPRO_PASSPHRASE"]
 
-    bucket = os.environ["GCS_BUCKET"]
     ticker = os.environ["TICKER"]
 
+    rolling_period = os.environ["ROLLING_PERIOD"]
+    increment = os.environ["INCREMENT"]
+    increment_int = int(increment)
 
-    auth_client = coin_utils.cbpro_auth()
-
+    # Read in buy history from storage
     buy_history = load_blob(project_id=project,
                             bucket_name=bucket,
                             destination_path=ticker,
                             filename="buy_history.pkl")
 
-    df = coin_utils.get_coin_data(ticker)
-    buy = coin_utils.buy_event(df, buy_history)
+    df = coin_utils.get_coin_data(ticker, int(rolling_period))
+    latest_record = len(df)-1
 
-    if buy[0] and auth_client.get_accounts()[2].get("available")>=100:
-        auth_client.place_market_order(product_id='BTC-USD',
-                               side='buy',
-                               funds='100.00')
+    # buy event
+    if df.iloc[latest_record,:]["close"] <= df.iloc[latest_record,:]["rolling_min"]:
+        # auth first
+        auth_client = coin_utils.cbpro_auth(key,secret,passphrase)
+        if auth_client.get_accounts()[2].get("available")>=increment_int:
+            # place order
+            auth_client.place_market_order(product_id='{}-USD'.format(ticker.upper()),
+                                   side='buy',
+                                   funds='{}.00'.format(increment))
 
+            # record buy and upload blob to storage
+            buy = int(increment)/df.iloc[latest_record,:]["close"]
+            buy_history.append(buy)
+
+            # temp storage for lambda
+            pickle.dump(buy_history, open("tmp/buy_history.pkl", "wb"))
+
+            # util function to upload buy history to storage
+            upload_blob(project_id=project,
+                            bucket_name=bucket,
+                            source_file_name="buy_history.pkl",
+                            destination_blob_name="{}/buy_history.pkl".format(ticker))
+
+    # sell event
+    if df.iloc[latest_record,:]["close"] >= df.iloc[latest_record,:]["rolling_max"] and len(buy_history)>0:
+        # auth first
+        auth_client = coin_utils.cbpro_auth(key,secret,passphrase)
+        # get the sell amount as oldest buy amount
+        btc_amount = buy_history.pop(0)
+        # calculate sell the dollar value
+        sell = btc_amount*df.iloc[latest_record,:]["close"]
+        # only sell if the amount is greater than your buy increments
+        if sell > increment_int:
+            auth_client.place_market_order(product_id='BTC-USD',
+                               side='sell',
+                               size=sell)
+        else:
+            buy_history.insert(0,btc_amount)
     return True
